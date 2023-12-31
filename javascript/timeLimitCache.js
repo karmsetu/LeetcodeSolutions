b88991764298a7912a38e22df299836becfe9fa3
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2024-01-01 03:31:27 +0530

    setter func completed

--- javascript/timeLimitCache.js
+++ javascript/timeLimitCache.js
@@ -1,180 +1,67 @@
 var TimeLimitedCache = function() {
-    let arrKeyVal = []
-    this.arrKeyVal = arrKeyVal
-    console.log(`class initiated`)
+    this.memory = []
 };
-
-/** 
- * @param {number} key
- * @param {number} value
- * @param {number} duration time until expiration in ms
- * @return {boolean} if un-expired key already existed
- */
-
-/**
- *TODO: The method should return true if the same un-expired key already exists and false otherwise. Both the value and duration should be overwritten if the key already exists.
- */
 TimeLimitedCache.prototype.set = function(key, value, duration) {
-    const crrArr = this.arrKeyVal
-    const crrTime = new Date().getTime()
-    let result;
-    let hasKeyArr = crrArr.map(item=> Object.keys(item.pair)[0] === key).includes(true)
-    console.log({hasKeyArr})
-    if(crrArr.length === 0 || !hasKeyArr){
-        crrArr.push({
-                recordTime: new Date().getTime(),
-                duration: duration,
-                pair: {[key]: value}
-            })
-        result = false
+    let setMemory = this.memory
+    let resultToBeReturned = false;
+    const currTime = new Date().getTime()
+    const hasKey = this.memory.map(item=> {return Object.keys(item.pair)[0] === String(key)}).includes(true)
+    if(setMemory.length ===0 || !hasKey){
+        this.memory.push({
+            duration,
+            entryTime: new Date().getTime(),
+            pair: {[key]: value}
+        })
     } else {
-
-        crrArr.forEach(entry=> {
-            const pair = entry.pair
-            console.log({entry, pair})
-        console.log(`forEach`)
-        const condition = (crrTime-entry.recordTime)<= entry.duration
-        const objKey = Object.keys(entry.pair)[0]
-        const strKey = String(key)
-        console.log({objKey, strKey}, typeof(objKey), typeof(strKey))
-        if (Object.keys(entry.pair)[0] === String(key)) {
-            console.log(`key already exists`)
-            entry.pair[key] = value;
-            entry.duration = duration;
-            entry.recordTime = new Date().getTime()
-            if (condition) {
-                result= true
-            } else result= false
-        } 
-        // else {
-        //     result = false
-        //     crrArr.push({
-        //         recordTime: new Date().getTime(),
-        //         duration: duration,
-        //         pair: {[key]: value}
-        //     })
-        // }
-    })
-    console.log({key, value, duration,crrArr})
-}
-return result
-};
-
-/** 
- * @param {number} key
- * @return {number} value associated with key
- */
-TimeLimitedCache.prototype.get = function(key) {
-    let result;
-    const crrTime = new Date().getTime()
-    result = this.arrKeyVal.map(entry=> {
-        const recordTime = entry.recordTime
-        const condition = (crrTime - recordTime)<= entry.duration
-        const duration =  entry.duration
-        const timeElapsed = crrTime - recordTime
-        const checkingPair =Object.keys(entry.pair)[0]
-        console.log({crrTime,condition, recordTime, duration, timeElapsed, checkingPair, key})
-        if (checkingPair === String(key)) {
-            console.log(`success`)
-            if (condition) {
-                console.log(`found the key: ${entry.pair[key]} `)
-                return entry.pair[key]
-            } else {
-                console.log(`the key has elapsed`)
-                return -1
+        setMemory.forEach(entry=> {
+            if (Object.keys(entry.pair)[0]=== String(key)) {
+                if ((currTime-entry.entryTime)<= duration) {
+                    resultToBeReturned = true
+                }
+                entry.entryTime = new Date().getTime()
+                entry.pair[key] = value
+                entry.duration = duration 
             }
-        } else {
-            console.log(`Key was not found in first place`)
-            return -1
-        }
-    })
-    const trueResult = result.filter(item=> item!=(-1))[0]
-    return trueResult ? trueResult : -1
+        })
+        this.memory = setMemory
+    }
+    console.log(this.memory)
+    return resultToBeReturned
+};
+TimeLimitedCache.prototype.get = function(key) {
+    
 };
 
-/** 
- * @return {number} count of non-expired keys
- */
 TimeLimitedCache.prototype.count = function() {
-    var count = 0
-    const crrTime = new Date().getTime()
-    this.arrKeyVal.forEach(entry=> {
-        if ((crrTime-entry.recordTime)<= entry.duration) {
-            console.log(`count++`)
-            console.log(`
-                duration${entry.duration}
-                recordTime${entry.recordTime}
-                pair:${Object.keys(entry.pair)}:${Object.values(entry.pair)}
-            `)
-            count++
-        }
-    })
-    console.log({count})
-    return count
+    
 };
 
-/**
- */
-
-/**
- * TODO: Write a class that allows getting and setting key-value pairs, however a time until expiration is associated with each key.
-*/
 
 const timeLimitedCache = new TimeLimitedCache()
-// timeLimitedCache.set(1, 42, 1000); // false
-// setTimeout(()=> {
-//     const result = timeLimitedCache.get(1)
-//     console.log({result})
-// }, 20)
-// timeLimitedCache.count() // 1
-// 42
-
-// ["TimeLimitedCache", "set", "set", "get", "get", "count"]
-// [[], [123456789, 987654321, 250], [123456788, 987654320, 100], [123456788], [123456789], []]
-// [0, 0, 0, 50, 150, 300]
-
-// const setting1 = timeLimitedCache.set(123456789, 987654321, 250)
-// const setting2 = timeLimitedCache.set(123456788, 987654320, 100)
-// console.log({setting1})
-// console.log({setting2})
-// setTimeout(()=> {
-//     const getting1 = timeLimitedCache.get(123456788)
-//     console.log({getting1})
-// }, 50)
-// setTimeout(()=> {
-//     const getting2 = timeLimitedCache.get(123456789)
-//     console.log({getting2})
-// }, 150)
-// setTimeout(()=> {
-//     const counting = timeLimitedCache.count()
-//     console.log({counting})
-// }, 300)
-// ["TimeLimitedCache", "set", "set", "set", "get", "get", "get", "count", "count"]
-// [[], [100, 200, 300], [200, 300, 300], [300, 400, 300], [100], [200], [300], [], []]
-// [0, 0, 0, 0, 100, 100, 100, 200, 400]
-timeLimitedCache.set(100, 200, 300)
+timeLimitedCache.set(100, 200, 400)
 timeLimitedCache.set(200, 300, 300)
-timeLimitedCache.set(300, 400, 300)
-setTimeout(()=> {
-    const get= timeLimitedCache.get(100)
-    console.log(get)
-}, 100)
-setTimeout(()=> {
-    const get= timeLimitedCache.get(200)
-    console.log(get)
-}, 100)
-setTimeout(()=> {
-    const get= timeLimitedCache.get(300)
-    console.log(get)
-}, 100)
-setTimeout(()=> {
-    const count= timeLimitedCache.count()
-    console.log(count)
-}, 200)
-setTimeout(()=> {
-    const count= timeLimitedCache.count()
-    console.log(count)
-}, 400)
+timeLimitedCache.set(200, 500, 500)
+// timeLimitedCache.set(300, 400, 300)
+// setTimeout(()=> {
+//     const get= timeLimitedCache.get(100)
+//     console.log(get)
+// }, 100)
+// setTimeout(()=> {
+//     const get= timeLimitedCache.get(200)
+//     console.log(get)
+// }, 100)
+// setTimeout(()=> {
+//     const get= timeLimitedCache.get(300)
+//     console.log(get)
+// }, 100)
+// setTimeout(()=> {
+//     const count= timeLimitedCache.count()
+//     console.log(count)
+// }, 200)
+// setTimeout(()=> {
+//     const count= timeLimitedCache.count()
+//     console.log(count)
+// }, 400)
 
 
 // TODO : RETURN the val with `map` function, dont use for each
