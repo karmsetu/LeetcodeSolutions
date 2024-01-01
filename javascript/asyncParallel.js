4cbf508320744b7c855ff2726b6fcff21e11cd18
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2024-01-01 18:12:22 +0530

    JS: asyncParallel; using forEach

--- javascript/asyncParallel.js
+++ javascript/asyncParallel.js
@@ -1,39 +1,44 @@
+// var promiseAll = async function(functions) {
+//     let newPromise = new Promise(async(resolve, reject)=>{
+//         let result = []
+//         var isError = false
+//         for (let index = 0; index < functions.length; index++) {
+//             const fn = await functions[index]()
+//                 .then(res=> result.push(res))
+//                 .catch(error => {reject(error)})
+//                 resolve(result)
+//             }
+//         })
+//     // console.log({newPromise})
+//     return newPromise
+// };
+
 var promiseAll = async function(functions) {
-    let newPromise = new Promise(async(resolve, reject)=>{
-        let result = []
-        var isError = false
-        for (let index = 0; index < functions.length; index++) {
-            try {
-                const fn = functions[index]
-                console.log(fn)
-                const functionToBeReturned = await fn()
-                result.push(functionToBeReturned)
-                console.log(result)
-            } catch (error) {
-                isError = true
-                reject(error)
-                break
-            }
-        }
-        if(!isError) {resolve(result)}
-    }).catch(e => {
-        console.error(e)
-        return e
-    })
-    return newPromise
+    return new Promise((resolve, reject) => {
+        // We know the resulting array will be the same length as functions
+        const results = new Array(functions.length);
+        let count = 0;
+        functions.forEach((fn, i) => {
+            fn()
+            .then(val => {
+                results[i] = val;
+                count++;
+                if(count === functions.length) resolve(results);
+            })
+            .catch(reason => reject(reason));
+        });
+    });
 };
 
 const functions = [
     () => new Promise(resolve => setTimeout(() => resolve(5), 200)),
     () => new Promise(resolve => setTimeout(() => resolve(1), 200)), 
-    () => new Promise((resolve, reject) => setTimeout(() => reject("Error"), 100)),
+    // () => new Promise((resolve, reject) => setTimeout(() => reject("Error"), 100)),
     () => new Promise(resolve => setTimeout(() => resolve(4), 50)), 
     () => new Promise(resolve => setTimeout(() => resolve(10), 150)), 
     () => new Promise(resolve => setTimeout(() => resolve(16), 100))
 ]
 
-try {
-    promiseAll(functions)
-} catch (error) {
-    console.error({error})
-}
+
+const result =  promiseAll(functions).then(res=> console.log(res)).catch(e => console.error(e))
+// console.log({result})?
